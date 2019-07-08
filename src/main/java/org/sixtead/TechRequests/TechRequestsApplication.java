6158241bbbd2c47fd7dd5adb4533a71937d675bd
sixtead <sixtead@gmail.com>
2019-07-08 21:25:17 +0300

    Initial commit

---
+++ src/main/java/org/sixtead/TechRequests/TechRequestsApplication.java
@@ -0,0 +1,13 @@
+package org.sixtead.TechRequests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechRequestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechRequestsApplication.class, args);
+	}
+
+}
