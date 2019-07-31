8005226db257155eb43f4819d38104f1a8a3d6f2
sixtead <sixtead@gmail.com>
2019-07-31 17:16:20 +0300

    Experiments with login

---
+++ src/main/java/org/sixtead/techrequests/login/LoginController.java
@@ -0,0 +1,13 @@
+package org.sixtead.techrequests.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "login";
+    }
+}
--- src/main/java/org/sixtead/techrequests/spring/configuration/WebSecurityConfiguration.java
+++ src/main/java/org/sixtead/techrequests/spring/configuration/WebSecurityConfiguration.java
@@ -1,20 +1,68 @@
 package org.sixtead.techrequests.spring.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+
+        String usersByUsername =
+                "SELECT username, password_digest AS password, enabled " +
+                "FROM users WHERE username = ?";
+        String authoritiesByUsername =
+                "SELECT users.username, roles.name " +
+                "FROM users, roles, group_roles " +
+                "WHERE users.username = ? " +
+                "AND group_roles.group_id = users.group_id " +
+                "AND roles.id = group_roles.role_id";
+
+//        auth
+//                .inMemoryAuthentication()
+//                .withUser("user")
+//                    .password(passwordEncoder.encode("user"))
+//                    .roles("USER")
+//                .and()
+//                .withUser("admin")
+//                    .password(passwordEncoder.encode("admin"))
+//                    .roles("ADMIN");
+
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .usersByUsernameQuery(usersByUsername)
+                .authoritiesByUsernameQuery(authoritiesByUsername)
+                .passwordEncoder(passwordEncoder);
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().anonymous();
+        http
+            .authorizeRequests()
+//                .anyRequest().permitAll()
+                .antMatchers("/webjars/**").permitAll()
+                .antMatchers("/groups/**").hasAnyRole("ADMIN")
+                .antMatchers("/users/**").hasAnyRole("USER", "ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                    .loginPage("/login")
+                    .permitAll()
+                .and()
+                    .logout()
+                    .permitAll();
+//                .httpBasic();
     }
 }
